d87036474936bbccd4847dda07954533114083ab
Starship100 <jimmy.widlund@outlook.com>
2026-02-21 21:11:15 +0100

    testmapp och test fil

--- examples.py
+++ examples.py
@@ -1,3 +1,9 @@
+from math import sqrt
+
+
 def add(x, y):
     return x + y
     #return None
+
+def hypo(a, b):
+    return sqrt(a * a + b * b)
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,6 +1,12 @@
-from examples import add
+from examples import add, hypo
 
 def test_add():
     expected = 3
     actual = add(1, 2)
+    assert actual == expected
+
+def test_hypo():
+    # testdata: a=5, b=12, =13
+    expected = 13
+    actual = hypo(5, 12)
     assert actual == expected
